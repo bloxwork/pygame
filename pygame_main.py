1d938d4a22540421efd1f2c9abb758df01e09c88
goblin <aleblo@web.de>
2018-09-17 21:30:32 +0200

    Initiale Version

---
+++ pygame_main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Kleines Text Adventure
+   Hier lehrnen wir python
+
+   von Ben & Sascha
+"""
+
+
+print("______ ___.__. _________    _____   ____  \n"  
+      "\____ <   |  |/ ___\__  \  /     \_/ __ \ \n" 
+      "|  |_> >___  / /_/  > __ \|  Y Y  \  ___/ \n" 
+      "|   __// ____\___  (____  /__|_|  /\___  >\n" 
+      "|__|   \/   /_____/     \/      \/     \/ \n"
+      "                          by Ben & Sascha \n")
